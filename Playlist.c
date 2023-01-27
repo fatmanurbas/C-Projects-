7d05b947eca36a804cc8d684e4669fe7c3965ace
fatmanurbas <69979408+fatmanurbas@users.noreply.github.com>
2023-01-27 19:29:38 +0300

    Update and rename Mixtape.c to Playlist.c

--- Playlist.c
+++ Playlist.c
@@ -1,5 +1,5 @@
 /*
-About MixtapeGenerator.exe : It is a project that I made for "BLM1012 Introduction To Procedural Programming" class back in 2020. 
+About Playlist.c: It is a school project that I made for "BLM1012 Introduction To Procedural Programming" class back in 2020. 
 Containing 55 different songs, 6 different moods and 4 scales it gives user a 13 songs playlist based on the moods and scales.
 */
 
