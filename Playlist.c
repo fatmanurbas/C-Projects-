07dc197a96b2c519b49998b80bdb6f62a4a096a2
fatmanurbas <69979408+fatmanurbas@users.noreply.github.com>
2023-05-21 22:04:30 +0300

    Update Playlist.c

--- Playlist.c
+++ Playlist.c
@@ -1,6 +1,6 @@
 /*
 About Playlist.c: It is a school project that I made for "BLM1012 Introduction To Procedural Programming" class back in 2020. 
-Containing 55 different songs, 6 different moods and 4 scales it gives user a 13 songs playlist based on the moods and scales.
+Containing 55 different songs, 6 different moods and 4 scales. It gives user a 13 songs playlist based on the moods and scales.
 */
 
 
